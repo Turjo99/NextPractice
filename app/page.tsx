c6c0bd2f6f97c5b689904d3f18554c328f6df397
Turjo99 <imturjo27@gmail.com>
2023-12-12 11:39:29 +0600

    learned fetching data without useState and useEffect hook

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Nav from "./Nav/Nav";
 import Link from "next/link";
 import Card from "./components/Card";
+import Post from "./components/Post";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Link href="/users">Click hereee</Link>
       <Nav></Nav>
       <Card></Card>
+      <Post></Post>
     </>
   );
 }
