90e39240c821aba376672863f122a2f12c4a1c8e
Turjo99 <imturjo27@gmail.com>
2023-12-31 12:02:24 +0600

    playing with layout system

---
+++ app/dashboard/layout.js
@@ -0,0 +1,10 @@
+const dashlayout = ({ children }) => {
+  return (
+    <div>
+      <div className="">side</div>
+      {children}
+    </div>
+  );
+};
+
+export default dashlayout;
--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,16 @@ import Image from "next/image";
 import Nav from "./Nav/Nav";
 import Link from "next/link";
 import Card from "./components/Card";
-import Post from "./components/Post";
+import Posts from "./components/Posts";
 
 export default function Home() {
   return (
     <>
       <h1>hello</h1>
       <Link href="/users">Click hereee</Link>
-      <Nav></Nav>
+
       <Card></Card>
-      <Post></Post>
+      <Posts></Posts>
     </>
   );
 }
